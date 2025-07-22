98cfa2659582e2dd12f000a5aa4aed24b59ccdde
AzureAD\MartijnSchimmel <m.schimmel@adapcare.nl>
2025-07-22 08:29:43 +0200

    Enhance `Meta` class with validation decorators and rename variable in `index.ts` for clarity.

--- src/resources/domain-resource.ts
+++ src/resources/domain-resource.ts
@@ -1,16 +1,21 @@
 import {Resource} from "./resource";
 import {Narrative} from "../elements/narrative";
 import {Extension} from "../elements/extension";
+import {Type} from 'class-transformer'
 
 export class DomainResource extends Resource {
 
+    @Type(() => Narrative)
     text: Narrative = new Narrative();
-
-    contained?: Resource
-
-    extension?: Extension
-
-    modifierExtension?: Extension
+    
+    @Type(() => Resource)
+    contained: Resource[] = []
+    
+    @Type(() => Extension)
+    extension: Extension[] = []
+    
+    @Type(() => Extension)
+    modifierExtension: Extension[] = []
 
     constructor() {
         super();
